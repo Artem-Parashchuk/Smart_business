ce92cb1182d21b61facee554cc89f3f331c2bab6
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-09-02 15:58:37 +0300

    update Loader component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,15 +1,13 @@
 import { InfinitySpin } from "react-loader-spinner";
-
 import s from "./Loader.module.css";
 
-export const Loader = (): JSX.Element => {
-  return (
+
+export const Loader = (): JSX.Element | null => {
+  return  (
     <div className={s.loader}>
       <InfinitySpin
-        visible ={true}
         width="200"
         color="#4fa94d"
-        ariaLabel="infinity-spin-loading"
       />
     </div>
   );
