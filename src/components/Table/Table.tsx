74bf8cf44339df4086a6f8074ffa52b2f8e5bcc8
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-09-03 14:23:32 +0300

    refactoring Table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -7,7 +7,6 @@ import { clearFilters, setFilter, User } from "../../redux/usersSlice";
 import {
   selectError,
   selectFilteredUsers,
-  selectFilters,
   selectLoading,
 } from "../../redux/selectors";
 import { UserFilters } from "../../redux/usersSlice";
@@ -17,13 +16,13 @@ import { Loader } from "../Loader/Loader";
 import { ModalWindow } from "../ModalWindow/ModalWindow";
 
 import s from "./Table.module.css";
+import { TableHead } from "../TableHead/TableHead";
 
 export const Table = () => {
   const dispatch = useDispatch<AppDispatch>();
   const filteredUsers = useSelector(selectFilteredUsers);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
-  const filters = useSelector(selectFilters);
 
   const [selectedUser, setSelectedUser] = useState<null | User>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -56,7 +55,11 @@ export const Table = () => {
     return <Loader />;
   }
   if (error) {
-    return <p>Error: {error}</p>;
+    return (
+      <p className={s.error_message}>
+        Sorry, something went wrong. We are working on it.
+      </p>
+    );
   }
 
   return (
@@ -65,56 +68,7 @@ export const Table = () => {
         <div className={s.table_container}>
           <table>
             <thead>
-              <tr>
-                <td>Name</td>
-                <td>Username</td>
-                <td>Email</td>
-                <td>Phone</td>
-              </tr>
-              <tr>
-                <td>
-                  <input
-                    className={s.input}
-                    type="text"
-                    placeholder="Search name"
-                    value={filters.name}
-                    onChange={(e) => handleFilterChange("name", e.target.value)}
-                  />
-                </td>
-                <td>
-                  <input
-                    className={s.input}
-                    type="text"
-                    placeholder="Search username"
-                    value={filters.username}
-                    onChange={(e) =>
-                      handleFilterChange("username", e.target.value)
-                    }
-                  />
-                </td>
-                <td>
-                  <input
-                    className={s.input}
-                    type="text"
-                    placeholder="Search email"
-                    value={filters.email}
-                    onChange={(e) =>
-                      handleFilterChange("email", e.target.value)
-                    }
-                  />
-                </td>
-                <td>
-                  <input
-                    className={s.input}
-                    type="text"
-                    placeholder="Search phone"
-                    value={filters.phone}
-                    onChange={(e) =>
-                      handleFilterChange("phone", e.target.value)
-                    }
-                  />
-                </td>
-              </tr>
+              <TableHead handleFilterChange={handleFilterChange}/>
             </thead>
 
             <tbody>
